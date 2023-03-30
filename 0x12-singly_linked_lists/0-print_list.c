dd8834c0d7a8929ea265ee2662e5193d71d214fb
Mostafa Muhamed <gekaa31@gmail.com>
2023-03-30 13:22:07 -0700

    Task one

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,21 +7,22 @@
  *
  * Return: The number of nodes in h.
  */
+
 size_t print_list(const list_t *h)
 {
-    const list_t *current = h;
-    size_t count = 0;
+	size_t nodes = 0;
 
-    while (current != NULL)
-    {
-        if (current->str != NULL)
-            printf("[%d] %s\n", current->len, current->str);
-        else
-            printf("[0] (nil)\n");
-        count++;
-        current = current->next;
-    }
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
 
-    return (count);
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
+		nodes++;
+		h = h->next;
+	}
+
+	return (nodes);
 }
-
