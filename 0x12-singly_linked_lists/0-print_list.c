339c4ceea7f86adf2c7606701392445307202288
Mostafa Muhamed <gekaa31@gmail.com>
2023-03-30 14:06:24 -0700

    Edit files

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,26 +1,28 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
- * print_list - prints all the elements of a list_t list
- * @h: pointer to head of the list
+ * print_list - Prints all the elements of a list_t list.
+ * @h: The list_t list.
  *
- * Return: the number of nodes
+ * Return: The number of nodes in h.
  */
+
 size_t print_list(const list_t *h)
 {
-    size_t count = 0;
+	size_t nodes = 0;
 
-    while (h != NULL)
-    {
-        if (h->str != NULL)
-            printf("[%u] %s\n", h->len, h->str);
-        else
-            printf("[0] (nil)\n");
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
 
-        count++;
-        h = h->next;
-    }
+		else
+			printf("[%d] %s\n", h->len, h->str);
 
-    return (count);
+		nodes++;
+		h = h->next;
+	}
+
+	return (nodes);
 }
-
--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,37 +1,27 @@
-#include <stdlib.h>
-#include <string.h>
 #include "lists.h"
-
 /**
- * add_node - Add a new node at the beginning of a list_t list.
- * @head: A pointer to a pointer to the first node of the list.
- * @str: The string to be added to the new node.
- *
- * Return: If memory allocation fails or head is NULL, NULL.
- *         Otherwise, the address of the new node.
+ * add_node - adds a new node at the beginning
+ * of a list_t list.
+ * @head: head of the linked list.
+ * @str: string to store in the list.
+ * Return: address of the head.
  */
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t *new_node;
+	list_t *new;
+	size_t a = 0;
 
-    if (head == NULL || str == NULL)
-        return (NULL);
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
 
-    new_node = malloc(sizeof(list_t));
-    if (new_node == NULL)
-        return (NULL);
+	while (str[a])
+		a++;
 
-    new_node->str = strdup(str);
-    if (new_node->str == NULL)
-    {
-        free(new_node);
-        return (NULL);
-    }
+	new->str = strdup(str);
+	new->len = a;
+	new->next = *head;
+	*head = new;
 
-    new_node->len = strlen(str);
-    new_node->next = *head;
-    *head = new_node;
-
-    return (new_node);
+	return (*head);
 }
-
