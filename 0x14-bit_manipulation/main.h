4fd435e2aab0870823fd83833952eab51c91ee97
Mostafa Muhamed <gekaa31@gmail.com>
2023-04-07 20:08:03 -0700

    edit

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -6,9 +6,12 @@
 
 /* Prototypes */
 int _putchar(char c);
-void *malloc_checked(unsigned int b);
-char *string_nconcat(char *s1, char *s2, unsigned int n);
-void *_calloc(unsigned int nmemb, unsigned int size);
-int *array_range(int min, int max);
-
-#endif
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+void print_number(int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
+#endif /* MAIN_H */
