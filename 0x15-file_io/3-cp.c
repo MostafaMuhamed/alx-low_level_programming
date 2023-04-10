4af0c6bdbe28119e7e5235a521ff959da8ee1191
Mostafa Muhamed <gekaa31@gmail.com>
2023-04-09 18:09:34 -0700

    edit

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,11 +1,11 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
 
 #define BUFFER_SIZE 1024
+char *create_buffer(char *file)
 
+void close_file(int fd);
 /**
  * create_buffer - Allocates 1024 bytes for a buffer.
  * @file: The name of the file buffer is storing chars for.
